f5f2dd7fb1dbb930f7e97700a3cf25c5b1cbc4f9
Sebastian Erives <serivesmejia@gmail.com>
2024-10-28 15:42:35 -0600

    Remove logback from plugin

--- EOCVSimPlugin/src/main/kotlin/io/github/deltacv/papervision/plugin/ipc/EOCVSimIpcEngineBridge.kt
+++ EOCVSimPlugin/src/main/kotlin/io/github/deltacv/papervision/plugin/ipc/EOCVSimIpcEngineBridge.kt
@@ -24,7 +24,6 @@ import io.github.deltacv.papervision.engine.message.PaperVisionEngineMessage
 import io.github.deltacv.papervision.engine.message.PaperVisionEngineMessageResponse
 import io.github.deltacv.papervision.plugin.ipc.serialization.ipcGson
 import org.java_websocket.client.WebSocketClient
-import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.net.URI
 import java.nio.ByteBuffer
